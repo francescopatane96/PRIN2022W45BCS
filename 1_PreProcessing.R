25987c1f926e26374c4bea95153a1194d6b83085
Francesco Patane, MSc <francesco.patane@live.it>
2025-02-23 17:03:09 +0100

    Update 1_PreProcessing.R

--- 1_PreProcessing.R
+++ 1_PreProcessing.R
@@ -77,6 +77,7 @@ seurat <- IntegrateLayers(
   new.reduction = "harmony",
   verbose = FALSE
 )
+seurat <- JoinLayers(seurat)
 
 cc_file <- getURL("https://raw.githubusercontent.com/hbc/tinyatlas/master/cell_cycle/Homo_sapiens.csv") 
 cell_cycle_genes <- read.csv(text = cc_file)
